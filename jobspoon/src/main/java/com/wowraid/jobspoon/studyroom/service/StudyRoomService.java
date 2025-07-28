e7e2135765440ae34ac8a14032309de6cdca582f
Roto90 <slalala90@gmail.com>
2025-07-28 11:43:44 +0900

    feat [박태준] 사용자는 본인이 생성한 스터디 룸의 정보를 수정합니다.[JSAB-3]
    feat [박태준] 사용자는 본인이 생성한 스터디 룸의 정보를 수정합니다.[JSAB-3]

--- jobspoon/src/main/java/com/wowraid/jobspoon/studyroom/service/StudyRoomService.java
+++ jobspoon/src/main/java/com/wowraid/jobspoon/studyroom/service/StudyRoomService.java
@@ -68,11 +68,11 @@ public class StudyRoomService {
         return RegisterStudyRoomResponse.from(studyRoom);
     }
 
-    // 한번씩~~ 너를 지울때마다~~ 가슴이 아려와~~(이승기:삭제)
-    @Transactional
-    public void deleteStudyRoom(Long studyRoomId){ // 추후 Account currentUser 추가해야하리보
-        StudyRoom studyRoom = studyRoomRepository.findById(studyRoomId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 스터디를 찾을 수 없습니다. id=" + studyRoomId));
-        studyRoomRepository.delete(studyRoom);
-    }
+//    // 한번씩~~ 너를 지울때마다~~ 가슴이 아려와~~(이승기:삭제)
+//    @Transactional
+//    public void deleteStudyRoom(Long studyRoomId){ // 추후 Account currentUser 추가해야하리보
+//        StudyRoom studyRoom = studyRoomRepository.findById(studyRoomId)
+//                .orElseThrow(() -> new IllegalArgumentException("해당 스터디를 찾을 수 없습니다. id=" + studyRoomId));
+//        studyRoomRepository.delete(studyRoom);
+//    }
 }
