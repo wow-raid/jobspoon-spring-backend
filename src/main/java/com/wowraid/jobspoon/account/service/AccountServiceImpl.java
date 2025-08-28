22efce35404781b51e7670804bb75ed14ba68236
puppy1012 <puppy1012@naver.com>
2025-08-28 16:01:54 +0900

    refactor: [이승현] 신규 회원은 카카오 소셜 로그인을 통해 접근할 경우, 약관 동의 후 회원 가입을 진행합니다 [JSAB-29]
    기존 roletype이 user로 한정되어있어 admin으로 roletype 입력이 불가능하기에 해당 로직을 분리하여 roletype, logintype을 입력하는방식으로 리펙토링

--- src/main/java/com/wowraid/jobspoon/account/service/AccountServiceImpl.java
+++ src/main/java/com/wowraid/jobspoon/account/service/AccountServiceImpl.java
@@ -26,23 +26,21 @@ public class AccountServiceImpl implements AccountService {
     @Transactional
     public Optional<Account> createAccount(RegisterAccountRequest requestForm) {
 
-
         AccountRoleType accountRoleType = accountRoleTypeRepository.findByRoleType(RoleType.USER)
                 .orElseThrow(() -> new IllegalArgumentException("RoleType.USER 가 DB에 존재하지 않습니다"));
 
-
         LoginType loginType = requestForm.getLoginType();
+        return createAccountWithRoleType(accountRoleType,loginType);
+
+    }
+    public Optional<Account> createAccountWithRoleType(AccountRoleType accountRoleType, LoginType loginType) {
 
         log.info("로그인 타입 : {}", loginType);
-
         AccountLoginType accountLoginType = accountLoginTypeRepository.findByLoginType(loginType)
                 .orElseThrow(() -> new IllegalArgumentException("LoginType.%s 가 DB에 존재하지 않습니다".formatted(loginType)));
-
 
         Account account = new Account(accountRoleType, accountLoginType);
         accountRepository.save(account);
         return Optional.of(account);
-
-
     }
 }
