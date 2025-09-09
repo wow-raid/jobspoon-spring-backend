43ab7de7f4272eb50080fbfbd6da0e7ec48eb603
정은선 <jeong7653@naver.com>
2025-09-09 11:17:08 +0900

    feat: [정은선] 사용자는 내 출석률, 문제 풀이 수, 면접 횟수 등 활동 지표를 대시보드에서 확인할 수 있다 [JSAB-22]
    - WritingCountService / Impl 생성
    - StudyRoomRepository에 host.account.id 기준 count 메소드 추가
    - WritingCountResponse DTO 작성 (reviewCount, studyroomCount, commentCount, totalCount 포함)
    - UserDashboardController에 /writing/count API 추가
    - review/comment는 도메인 미구현으로 0 반환 처리

--- src/main/java/com/wowraid/jobspoon/user_dashboard/controller/UserDashboardController.java
+++ src/main/java/com/wowraid/jobspoon/user_dashboard/controller/UserDashboardController.java
@@ -3,10 +3,8 @@ package com.wowraid.jobspoon.user_dashboard.controller;
 import com.wowraid.jobspoon.user_dashboard.controller.response_form.AttendanceRateResponse;
 import com.wowraid.jobspoon.user_dashboard.controller.response_form.InterviewCompletionResponse;
 import com.wowraid.jobspoon.user_dashboard.controller.response_form.QuizCompletionResponse;
-import com.wowraid.jobspoon.user_dashboard.service.AttendanceService;
-import com.wowraid.jobspoon.user_dashboard.service.InterviewSummaryService;
-import com.wowraid.jobspoon.user_dashboard.service.QuizSummaryService;
-import com.wowraid.jobspoon.user_dashboard.service.TokenAccountService;
+import com.wowraid.jobspoon.user_dashboard.controller.response_form.WritingCountResponse;
+import com.wowraid.jobspoon.user_dashboard.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +21,7 @@ public class UserDashboardController {
     private final AttendanceService attendanceService;
     private final InterviewSummaryService interviewSummaryService;
     private final QuizSummaryService quizSummaryService;
+    private final WritingCountService writingCountService;
 
     @GetMapping("/attendance/rate")
     public ResponseEntity<AttendanceRateResponse> getRate(@RequestHeader("Authorization") String userToken){
@@ -41,12 +40,30 @@ public class UserDashboardController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("quiz/completion")
+    @GetMapping("/quiz/completion")
     public ResponseEntity<QuizCompletionResponse> getQuizCompletion(@RequestHeader("Authorization") String userToken){
         Long accountId = tokenAccountService.resolveAccountId(userToken);
         QuizCompletionResponse response = new QuizCompletionResponse(
                 quizSummaryService.getTotalCount(accountId),
                 quizSummaryService.getMonthlyCount(accountId)
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/writing/count")
+    public ResponseEntity<WritingCountResponse> getWritingCount(@RequestHeader("Authorization") String userToken){
+        Long accountId = tokenAccountService.resolveAccountId(userToken);
+
+        long reviewCount = writingCountService.getReviewCount(accountId);
+        long studyroomCount = writingCountService.getStudyroomCount(accountId);
+        long commentCount = writingCountService.getCommentCount(accountId);
+
+        WritingCountResponse response = new WritingCountResponse(
+                reviewCount,
+                studyroomCount,
+                commentCount,
+                reviewCount + studyroomCount + commentCount   // 👈 총 작성 횟수
         );
 
         return ResponseEntity.ok(response);
