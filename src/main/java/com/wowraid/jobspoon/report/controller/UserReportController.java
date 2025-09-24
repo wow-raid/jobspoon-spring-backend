f1cfa6545788caf577a6c32c0ab7f8d755535395
Roto90 <slalala90@gmail.com>
2025-09-24 16:11:10 +0900

    chore: [박태준] 모임 내 신고 API 변경
    1. chore
    - 추후 있을 다른 도메인에서의 신고 기능과 중복되지 않도록 API 변경

--- src/main/java/com/wowraid/jobspoon/report/controller/UserReportController.java
+++ src/main/java/com/wowraid/jobspoon/report/controller/UserReportController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/reports")
+@RequestMapping("/api/study-rooms/reports")
 public class UserReportController {
 
     private final UserReportService userReportService;
