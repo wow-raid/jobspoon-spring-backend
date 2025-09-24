53464af6d20e2e082f159e5186c9de000c875fbc
초묨 <puppy1012@naver.com>
2025-09-24 23:00:51 +0900

    chore: [이승현] 과도하게 몰리는 관리자 엔드포인트 분리를 위한 컨트롤러 분리
    관리자 로그인관련 엔드포인트를 LoginController로
    관리자 유지보수 관련 엔드포인트를 managementController로 분리

--- src/main/java/com/wowraid/jobspoon/administer/controller/AdministratorLoginController.java
+++ src/main/java/com/wowraid/jobspoon/administer/controller/AdministratorLoginController.java
@@ -2,7 +2,6 @@ package com.wowraid.jobspoon.administer.controller;
 
 import com.wowraid.jobspoon.administer.controller.dto.AdministratorCodeLoginRequest;
 import com.wowraid.jobspoon.administer.controller.dto.AdministratorUserInfoRequest;
-import com.wowraid.jobspoon.administer.controller.dto.AdministratorUserInfoResponse;
 import com.wowraid.jobspoon.administer.service.AdministratorManagementService;
 import com.wowraid.jobspoon.administer.service.AdministratorService;
 import com.wowraid.jobspoon.administer.service.dto.AdministratorUserListResponse;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/administrator")
 @RequiredArgsConstructor
-public class AdministratorController {
+public class AdministratorLoginController {
 
     private final AdministratorService administratorService;
     private final AdministratorManagementService administratorManagementService;
@@ -58,17 +57,5 @@ public class AdministratorController {
                 ? ResponseEntity.ok().build() //200처리
                 : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //401 처리
     }
-    @PostMapping("/userinfo")
-    public ResponseEntity<AdministratorUserListResponse> getUserInfo(@RequestHeader("Authorization")  String userToken,
-                                                                     @RequestBody AdministratorUserInfoRequest request){
-        log.info("AdministratorController.getUserInfo is working");
-        boolean valid = administratorService.isAdminByUserToken(userToken.replace("Bearer ", "").trim());
-        if(!valid){
-            log.info("valid is not matched");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        AdministratorUserListResponse administratorUserListResponse = administratorManagementService.getUserInfo(request);
 
-        return ResponseEntity.ok(administratorUserListResponse);
-    }
 }
