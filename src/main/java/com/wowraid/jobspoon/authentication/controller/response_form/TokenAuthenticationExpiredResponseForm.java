ef7f6689810690dde3ed99ce712c9c110cf42e57
Hyunsoo Choi <gustn9025@naver.com>
2025-09-26 23:56:43 +0900

    fix: [최현수] 로그인 검증 객체 기본 생성자 누락 해결

--- src/main/java/com/wowraid/jobspoon/authentication/controller/response_form/TokenAuthenticationExpiredResponseForm.java
+++ src/main/java/com/wowraid/jobspoon/authentication/controller/response_form/TokenAuthenticationExpiredResponseForm.java
@@ -1,13 +1,15 @@
 package com.wowraid.jobspoon.authentication.controller.response_form;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class TokenAuthenticationExpiredResponseForm {
 
     private final boolean status;
-
+    
 
 }
