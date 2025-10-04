ee636ee092eb17a3a68660abcbdbd461969c71c6
Hyunsoo Choi <gustn9025@naver.com>
2025-10-04 19:02:46 +0900

    feat: [최현수] 회원가입 성공 시 로그인 했을 때와 같은 인증 쿠키를 함께 전달합니다 [JSAB-122]

--- src/main/java/com/wowraid/jobspoon/account/controller/AccountController.java
+++ src/main/java/com/wowraid/jobspoon/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.wowraid.jobspoon.account.controller.request_form.RegisterRequestForm;
 import com.wowraid.jobspoon.account.service.AccountService;
 import com.wowraid.jobspoon.account.service.SignupService;
 import com.wowraid.jobspoon.account.service.register_response.RegisterResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +21,27 @@ public class AccountController {
 
 
     @PostMapping("/signup")
-    public ResponseEntity<RegisterResponse> signup(@CookieValue(name="userToken", required = false) String temporaryUserToken,
-                                                   @RequestBody RegisterRequestForm registerRequestForm) {
+    public ResponseEntity<RegisterResponse> signup(
+            @CookieValue(name="userToken", required = false) String temporaryUserToken,
+            @RequestBody RegisterRequestForm registerRequestForm,
+            HttpServletResponse response) {   // <- response 추가
 
         log.info("Signup request - 회원가입 호출 완료");
 
+        // 회원가입 서비스 호출
         RegisterResponse signupResult = signupService.signup(temporaryUserToken, registerRequestForm);
+
+        // 회원가입 성공 후 새 userToken 쿠키 설정
+        String cookieHeader = String.format(
+                "userToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure; SameSite=Strict",
+                signupResult.getUserToken(),  // service에서 발급한 토큰 사용
+                6 * 60 * 60                   // 6시간
+        );
+        response.addHeader("Set-Cookie", cookieHeader);
 
         return ResponseEntity.ok(signupResult);
     }
+
 
 
 
