48534a5670c12a8b6253e2c41d02e3a510080fa7
Roto90 <slalala90@gmail.com>
2025-10-06 22:29:19 +0900

    feat: [박태준] 신고 파일 첨부를 위해 리포트 전용 S3 클라이언트 및 환경 변수를 구성합니다. [JSAB-130]
    1. feat
    - 리포트 전용 S3 클라이언트 및 환경변수 설정

--- src/main/java/com/wowraid/jobspoon/config/S3Config.java
+++ src/main/java/com/wowraid/jobspoon/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.wowraid.jobspoon.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,10 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${PROFILE_AWS_ACCESS_KEY_ID}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${PROFILE_AWS_SECRET_ACCESS_KEY}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
@@ -25,6 +26,7 @@ public class S3Config {
     private String bucket;
 
     @Bean
+    @Qualifier("s3Client")
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
@@ -47,4 +49,23 @@ public class S3Config {
                 )
                 .build();
     }
+
+        // --- 신고 파일용 S3 설정 ---
+        @Value("${REPORT_AWS_ACCESS_KEY_ID}")
+        private String reportAccessKey;
+
+        @Value("${REPORT_AWS_SECRET_ACCESS_KEY}")
+        private String reportSecretKey;
+
+        @Bean
+        @Qualifier("reportS3Client")
+        public S3Client reportS3Client() {
+            return S3Client.builder()
+                    .region(Region.of(region))
+                    .credentialsProvider(StaticCredentialsProvider.create(
+                                    AwsBasicCredentials.create(reportAccessKey, reportSecretKey)
+                            )
+                    )
+                    .build();
+        }
 }
