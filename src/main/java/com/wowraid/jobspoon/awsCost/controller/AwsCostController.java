353bff0b1e700ea26439bf5a992f87f79d01129f
초묨 <puppy1012@naver.com>
2025-10-11 23:38:11 +0900

    update: [이승현] 관리자는 AWS 월별 비용을 조회가능합니다 [JSAB-113]
    임시로 제거한 CookieValue값 읽기 및 관리자 권한 조회 기능 활성화

--- src/main/java/com/wowraid/jobspoon/awsCost/controller/AwsCostController.java
+++ src/main/java/com/wowraid/jobspoon/awsCost/controller/AwsCostController.java
@@ -5,6 +5,7 @@ import com.wowraid.jobspoon.awsCost.entity.AwsDailyCost;
 import com.wowraid.jobspoon.awsCost.service.AwsCostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
@@ -20,14 +21,14 @@ public class AwsCostController {
     @GetMapping("/daily")
     public ResponseEntity<List<AwsDailyCost>> dailyCostList(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end)
-//            @CookieValue(name = "userToken", required = false) String userToken)
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end,
+            @CookieValue(name = "userToken", required = false) String userToken)
         {
 
         if (!end.isAfter(start))return ResponseEntity.badRequest().build();
 
-//        if(!administratorService.isAdminByUserToken(userToken))
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        if(!administratorService.isAdminByUserToken(userToken))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         List<AwsDailyCost> result = awsCostService.getDailyTotalCost(start,end);
 
 
