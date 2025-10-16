afeab27195393fcd8a51f33098bac0a8b6df7aeb
min <wittgen0024@gmail.com>
2025-10-16 20:24:18 +0900

    feat: [김정민] 퀴즈 제출 응답에 정답 정보 추가 [JSAB-110]

--- src/main/java/com/wowraid/jobspoon/quiz/controller/response_form/SubmitQuizSessionResponseForm.java
+++ src/main/java/com/wowraid/jobspoon/quiz/controller/response_form/SubmitQuizSessionResponseForm.java
@@ -1,12 +1,16 @@
 package com.wowraid.jobspoon.quiz.controller.response_form;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SubmitQuizSessionResponseForm {
     private final Long sessionId;
     private final int total;
@@ -15,10 +19,18 @@ public class SubmitQuizSessionResponseForm {
     private final List<Item> details;
 
     @Getter
-    @RequiredArgsConstructor
+    @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Item {
         private final Long quizQuestionId;
-        private final Long selectedChoiceId;
-        private final boolean correct;
+        
+        private final Long selectedChoiceId;        // 단일선택 시
+        private final List<Long> selectedChoiceIds; // 다답형 대비
+
+        // 정답 보기 (단일/다답 모두 대응)
+        private final Long correctChoiceId;         // 정답이 1개인 경우
+        private final List<Long> correctChoiceIds;  // 정답이 여러 개인 경우
+
+        private final boolean correct;              // 이 문항에서 사용자가 맞췄는지 여부
     }
 }
